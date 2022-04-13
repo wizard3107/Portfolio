690e89038e9183e6edb7b5b60b9b34bcc60f39e0
wizard3107 <ashishsingh9720@gmail.com>
2022-04-13 22:29:29 +0530

    added correct linkedIn link

--- src/data/socialsData.js
+++ src/data/socialsData.js
@@ -1,6 +1,6 @@
 export const socialsData = {
     github: 'https://github.com/wizard3107',
-    linkedIn: 'www.linkedin.com/in/ashish-singh-507ua',
+    linkedIn: 'https://www.linkedin.com/in/ashish-singh-507ua/',
     instagram: 'https://www.instagram.com/___sanskarichora/',
 
 }
