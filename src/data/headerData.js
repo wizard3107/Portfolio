9ba8ff915e689b337296005be3940900cacccb44
wizard3107 <ashishsingh9720@gmail.com>
2022-05-15 14:50:40 +0530

    adde the remsume link  and made some changes in the css

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     title: "A Full Stack Developer",
     desciption: "Programming is not about what you know, Its about what you can figure out.",
     image: profile,
-    resumePdf: "https://www.canva.com/design/DAE4VZmaUEQ/mW5qP6QIk0hIStlSgesb4g/view?utm_content=DAE4VZmaUEQ&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton"
+    resumePdf: "https://drive.google.com/file/d/1LL9qtAkcuw2AVieoFXpS3n5Ba5HmHCdZ/view?usp=sharing"
 }
