62ab4d34331193dc528fd1c63d616ee71ed6b7db
Ashish Singh <92727239+wizard3107@users.noreply.github.com>
2025-04-16 23:11:29 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,5 +5,5 @@ export const headerData = {
     title: "A Full Stack Developer",
     desciption: "Programming is not about what you know, Its about what you can figure out.",
     image: profile,
-    resumePdf: "https://www.canva.com/design/DAF8pnx29h8/5CyMps8TDh8LaMmDbwVWPQ/view?utm_content=DAF8pnx29h8&utm_campaign=designshare&utm_medium=link&utm_source=editor"
+    resumePdf: "https://www.canva.com/design/DAGj8XfKk9M/0AvoOV1Y3Ejnty_kOTuU_Q/view?utm_content=DAGj8XfKk9M&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h01d8af6f81"
 }
